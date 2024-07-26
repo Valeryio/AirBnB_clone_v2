727739d8c5a52141e90b0fb5468295ca3ae1fe67
Valeryio <tommagoma23@gmail.com>
2024-07-26 15:35:05 +0000

    first update on the task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 
 """This is a root for the hbnb function"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+if __name__ == "__main__":
+    # run the app on 0.0.0.0:5000
+    app.run(debug=True, port=5000, host="0.0.0.0")
