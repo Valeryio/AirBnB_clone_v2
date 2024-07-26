c0b0574607cac62d11adacd80689d939c4495b55
Valeryio <tommagoma23@gmail.com>
2024-07-26 15:52:24 +0000

    commit on the task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""This flask application display an app with a parameter"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    text = text.replace("_", " ")
+    return f"C {escape(text)}"
+
+if __name__ == "__main__":
+    # run the app on 0.0.0.0:5000
+    app.run(debug=True, port=5000, host="0.0.0.0")
+
