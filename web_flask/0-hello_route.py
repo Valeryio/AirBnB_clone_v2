cba1070b2992ceac459895564524dbe0a4c00f7e
Valeryio <tommagoma23@gmail.com>
2024-07-26 15:31:03 +0000

    third update on the task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
-    return "<p>Hello HBNB!</p>"
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
     # run app in debug mode on port 5000
