b7e95a3b802cce49cc43c64ac2d787f7ed525a44
Valeryio <tommagoma23@gmail.com>
2024-07-26 15:29:54 +0000

    Second commit on the task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.run(debug=True, port=5000, host="0.0.0.0")
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
     return "<p>Hello HBNB!</p>"
+
+if __name__ == "__main__":
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000, host="0.0.0.0")
