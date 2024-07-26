d810717234509437eec38b447a4a6951124fcc64
Valeryio <tommagoma23@gmail.com>
2024-07-26 15:56:33 +0000

    commit on the task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -18,10 +18,16 @@ def hbnb():
     return "HBNB"
 
 
-@app.route("/c/<text>")
+@app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     text = text.replace("_", " ")
     return f"C {escape(text)}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    text = text.replace("_", " ")
+    return f"Python {escape(text)}"
 
 
 if __name__ == "__main__":
