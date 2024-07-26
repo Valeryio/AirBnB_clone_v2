aa6e8752e58cab5a2178735d02c0a5fbd9ccde47
Valeryio <tommagoma23@gmail.com>
2024-07-26 15:36:41 +0000

    third update on the task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,13 +7,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 if __name__ == "__main__":
     # run the app on 0.0.0.0:5000
