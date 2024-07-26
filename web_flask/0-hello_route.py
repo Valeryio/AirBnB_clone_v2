0ae2ef36cdea5f9c744b5f32564de5bbd1587f69
Valeryio <tommagoma23@gmail.com>
2024-07-26 14:58:15 +0000

    New update on the project of Flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""This module creates a minimal web application in Flask"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.run(port=5000, host="0.0.0.0")
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    return "<p>Hello HBNB!</p>"
