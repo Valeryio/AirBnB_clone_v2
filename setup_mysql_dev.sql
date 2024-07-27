6cb304697578100c90c7891a4d3685013d57d892
Valeryio <tommagoma23@gmail.com>
2024-07-27 11:07:13 +0000

    commit the task 2

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+
+CREATE USER "hbnb_dev"@"localhost" IDENTIFIED BY "hbnb_dev_pwd" IF NOT EXISTS "hbnb_dev";
+CREATE DATABASE "hbnb_dev_db" IF NOT EXISTS "hbnb_dev_db";
+GRANT ALL ON hbnb_dev_db.* TO "hbnb_dev"@"localhost";
+GRANT SELECT ON performance_schema TO "hbnb_dev"@"localhost";
+
