f56e00b368a1c6aa2ce99f45800652cfe2c83f97
Valeryio <tommagoma23@gmail.com>
2024-07-27 11:23:21 +0000

    another update on the task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,6 +3,7 @@
 CREATE USER "hbnb_dev"@"localhost" IDENTIFIED BY "hbnb_dev_pwd";
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 GRANT ALL ON hbnb_dev_db.* TO "hbnb_dev"@"localhost";
-GRANT SELECT ON performance_schema TO "hbnb_dev"@"localhost";
+
+GRANT SELECT ON performance_schema.* TO "hbnb_dev"@"localhost";
 
 FLUSH PRIVILEGES;
