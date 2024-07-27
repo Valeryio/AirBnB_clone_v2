ae7f7d977343b78bcbfacb9c5c96ef6fb3bd2f5b
Valeryio <tommagoma23@gmail.com>
2024-07-27 11:33:44 +0000

    update the task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,7 +3,7 @@
 
 CREATE USER "hbnb_test"@"localhost" IDENTIFIED BY "hbnb_test_pwd"
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-GRANT ALL ON hbnb_test_db.* TO "hbnb_test"@"localhost";
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO "hbnb_test"@"localhost";
 GRANT SELECT ON performance_schema.* TO "hbnb_test"@"localhost";
 
 FLUSH PRIVILEGES;
