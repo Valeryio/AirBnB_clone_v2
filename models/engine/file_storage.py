04b2e466219bb33d9cd7ab9fdf3799aaa0da215a
Valeryio <tommagoma23@gmail.com>
2024-07-27 13:14:57 +0000

    update the pop item

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,8 +59,11 @@ class FileStorage:
     def delete(self, obj=None):
         """Deletes new object content"""
         if obj is not None:
-            obj_id = str(obj.__class__.__name__) + obj.id
-            print("The ID : ", obj_id)
+            obj_id = str(obj.__class__.__name__) + "." + obj.id
+            # print("The ID : ", obj_id)
 
-            if obj in self.__objects:
-                pass
+            for key, value in self.__objects.items():
+                # print(key)
+                if obj_id == key:
+                    self.__objects.pop(key)
+                    break
