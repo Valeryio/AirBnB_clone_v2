f9e970a0664377ad26d2e1e1a41a2c4a32c6abb0
Valeryio <tommagoma23@gmail.com>
2024-07-27 11:21:43 +0000

    update the task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- THis script prepare a new database for
 -- using it for the SQLAlchemy project
 CREATE USER "hbnb_dev"@"localhost" IDENTIFIED BY "hbnb_dev_pwd";
-CREATE DATABASE IF NOT EXISTS "hbnb_dev_db";
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 GRANT ALL ON hbnb_dev_db.* TO "hbnb_dev"@"localhost";
 GRANT SELECT ON performance_schema TO "hbnb_dev"@"localhost";
 
