abdc734b372c5f741a5054bd5972928de8f61391
Valeryio <tommagoma23@gmail.com>
2024-07-27 12:13:41 +0000

    update the delete function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,11 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes new object content"""
+        obj_id = str(obj.classname) + obj.id
+        print("The ID : ", obj_id)
+
+        if obj in self.__objects:
+            pass
