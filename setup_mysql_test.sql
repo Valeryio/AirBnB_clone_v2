5243e52dafbb6d63aa70ab06020d409b4388ccff
Valeryio <tommagoma23@gmail.com>
2024-07-27 11:27:40 +0000

    commit the task 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- THis is a script to set up the database
+-- of the Database
+
+CREATE USER "hbnb_test"@"localhost" IDENTIFIED BY "hbnb_test_pwd"
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+GRANT ALL ON hbnb_test_db.* TO "hbnb_test"@"localhost";
+GRANT SELECT ON performance_schema.* TO "hbnb_test"@"localhost";
