1047770d56b20a9f120c318abb894e06078b548b
Valeryio <tommagoma23@gmail.com>
2024-08-14 13:22:25 +0000

    Update the default value of the passwd of a user

--- models/user.py
+++ models/user.py
@@ -9,7 +9,7 @@ class User(BaseModel, Base):
     __tablename__ = "users"
 
     email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False, default="NULL")
+    password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False, default="NULL")
     last_name = Column(String(128), nullable=False, default="NULL")
 
