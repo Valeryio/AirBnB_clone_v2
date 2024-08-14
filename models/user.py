a557dc323fcff1753e2c8ae8e8ca0bcbc04bd894
Valeryio <tommagoma23@gmail.com>
2024-08-14 13:18:29 +0000

    Update the class User
    I've added some properties to make it more usefull

--- models/user.py
+++ models/user.py
@@ -9,9 +9,9 @@ class User(BaseModel, Base):
     __tablename__ = "users"
 
     email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False, default="NULL")
+    first_name = Column(String(128), nullable=False, default="NULL")
+    last_name = Column(String(128), nullable=False, default="NULL")
 
     places = relationship("Place", back_populates="users")
     reviews = relationship("Review", back_populates="users")
