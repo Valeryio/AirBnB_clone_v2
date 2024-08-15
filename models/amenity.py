5027ab38d9a756b10ced4436b0f12ed73b82011d
Valeryio <tommagoma23@gmail.com>
2024-08-15 08:16:30 +0000

    Update the class amenity by adding the Static engine functionnalities

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" Amenity Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import *
 from sqlalchemy.orm import relationship
 
-class Amenity(BaseModel, Base):
-    __tablename__ = "amenities"
 
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   back_populates="amenities")
+HBNB_TYPE_STORAGE = os.getenv("HBNB_TYPE_STORAGE")
+
+if HBNB_TYPE_STORAGE == "db":
+    class Amenity(BaseModel, Base):
+        __tablename__ = "amenities"
+
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                       back_populates="amenities")
+else:
+    class Amenity(BaseModel):
+        name = ""
+
