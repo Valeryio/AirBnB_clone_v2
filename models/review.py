506adbe87eb22358548c8d0cf9b7fd7a68adafa6
Valeryio <tommagoma23@gmail.com>
2024-08-15 08:12:13 +0000

    Update the class review by adding the Static engine functionnalities

--- models/review.py
+++ models/review.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import *
 from sqlalchemy.orm import relationship
 
+HBNB_TYPE_STORAGE = os.getenv("HBNB_TYPE_STORAGE")
 
-class Review(BaseModel, Base):
-    """ Review classto store review information """
-    __tablename__ = "reviews"
+if HBNB_TYPE_STORAGE == "db":
+    class Review(BaseModel, Base):
+        """ Review classto store review information """
+        __tablename__ = "reviews"
 
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        text = Column(String(1024), nullable=False)
 
-    places = relationship("Place", back_populates="reviews")
-    users = relationship("User", back_populates="reviews")
+        places = relationship("Place", back_populates="reviews")
+        users = relationship("User", back_populates="reviews")
+else:
+    class Review(BaseModel):
+        """ Review class to store review information """
+        place_id = ""
+        user_id = ""
+        text = ""
+
