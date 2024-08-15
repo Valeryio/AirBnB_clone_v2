659ddf831039cb9b56bf29af7df45c8ab2ec842f
Valeryio <tommagoma23@gmail.com>
2024-08-15 08:13:16 +0000

    Update the class state by adding the Static engine functionnalities

--- models/state.py
+++ models/state.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import *
 from sqlalchemy.orm import relationship
 
 
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
+HBNB_TYPE_STORAGE = os.getenv("HBNB_TYPE_STORAGE")
 
-    name = Column(String(128), nullable=False)
+if HBNB_TYPE_STORAGE == "db":
+    class State(BaseModel, Base):
+        """ State class """
+        __tablename__ = "states"
 
-    cities = relationship("City", back_populates="states")
+        name = Column(String(128), nullable=False)
+
+        cities = relationship("City", back_populates="states")
+else:
+    class State(BaseModel):
+        """ State class """
+        name = ""
