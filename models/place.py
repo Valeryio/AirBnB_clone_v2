5fd89e3ace9eb7a4d35938a5ad21901d0a3013a2
Valeryio <tommagoma23@gmail.com>
2024-08-15 08:10:51 +0000

    Update the class place by adding the Static engine functionnalities

--- models/place.py
+++ models/place.py
@@ -1,39 +1,59 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import *
 from sqlalchemy.orm import relationship
 from models.amenity import Amenity
 
 
-class Place(BaseModel, Base):
-    """ A place to stay """
-    __tablename__ = "places"
+HBNB_TYPE_STORAGE = os.getenv("HBNB_TYPE_STORAGE")
 
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    amenity_id = Column(String(60), nullable=False)
+if HBNB_TYPE_STORAGE == "db":
+    class Place(BaseModel, Base):
+        """ A place to stay """
+        __tablename__ = "places"
 
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        amenity_id = Column(String(60), nullable=False)
 
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
 
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
 
-    cities = relationship("City", back_populates="places")
-    users = relationship("User", back_populates="places")
-    reviews = relationship("Review", back_populates="places")
-    amenities = relationship("Amenity", secondary="place_amenity",
-                             viewonly=False, back_populates="place_amenities")
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
 
-place_amenity = Table(
+        cities = relationship("City", back_populates="places")
+        users = relationship("User", back_populates="places")
+        reviews = relationship("Review", back_populates="places")
+        amenities = relationship("Amenity", secondary="place_amenity",
+                                 viewonly=False, back_populates="place_amenities")
+
+
+    place_amenity = Table(
         "place_amenity",
-         Base.metadata,
-         Column("place_id", String(60), ForeignKey("places.id"), primary_key=True, nullable=False),
-         Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False)
-)
+        Base.metadata,
+        Column("place_id", String(60), ForeignKey("places.id"), primary_key=True, nullable=False),
+        Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False)
+    )
+
+else:
+    class Place(BaseModel):
+        """ A place to stay """
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
